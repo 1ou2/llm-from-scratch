bd98a18f151430cbbf6b4573c44339bbcf74a9a1
gabriel pastor <github@1ou2.com>
2024-07-03 20:26:43 +0200

    installation

--- mathtest.py
+++ mathtest.py
@@ -6,8 +6,6 @@ def f(x):
     return 3*x**2 - 4*x +3
 
 def hyperbole():
-
-
     xs = np.arange(-5,5,0.25)
     print(xs)
     ys = f(xs)
@@ -20,5 +18,6 @@ def derivative(x):
     # h doit tendre vers 0
     h = 0.0001
     return (f(x+h)-f(x))/h
+hyperbole()
 print(f(3.0))
 print(derivative(3.0))
