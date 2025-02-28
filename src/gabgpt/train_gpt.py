ab9945e86c9f54ea724e505a1c7d78b2a71d30ed
gabriel pastor <github@1ou2.com>
2025-02-28 14:44:51 +0100

    torch.compile

--- src/gabgpt/train_gpt.py
+++ src/gabgpt/train_gpt.py
@@ -1,7 +1,6 @@
 
 from model_gpt import GPTModel
 from dataloader import DataLoaderLite
-from stats_training import TrainingStats
 from transformers import get_linear_schedule_with_warmup
 import torch
 import tiktoken
@@ -64,12 +63,12 @@ def generate_text_completion(model, text):
     
     return tokenizer.decode(idx.squeeze().tolist())
 
-def save_checkpoint(model, optimizer, scheduler, train_loader, config, epoch, step, loss, stats, save_dir):
+def save_checkpoint(model, optimizer, scheduler, train_loader, config, epoch, step, loss, save_dir):
     """
     Save the model, optimizer, scheduler, and epoch to a file.
     """
     os.makedirs(save_dir, exist_ok=True)
-    path = f"{save_dir}/checkpoint_{epoch}_{step}_{loss:.2f}.pt"
+    path = f"{save_dir}/checkpoint_{epoch}_{step:07d}_{loss:.2f}.pt"
     checkpoint = {
         'epoch': epoch,
         'step': step,
@@ -78,8 +77,7 @@ def save_checkpoint(model, optimizer, scheduler, train_loader, config, epoch, st
         'scheduler_state_dict': scheduler.state_dict(),
         'loss': loss,
         'config': config,
-        'train_loader_state': train_loader.get_state(),
-        "stats_file": stats.save_stats(save_dir)
+        'train_loader_state': train_loader.get_state()
     }
     torch.save(checkpoint, path)
 
@@ -91,7 +89,29 @@ def load_checkpoint(path, model, optimizer=None, scheduler=None, device="cpu"):
     Raises ValueError in case of error
     """
     checkpoint = torch.load(path,map_location=device,weights_only=True)
-    model.load_state_dict(checkpoint['model_state_dict'])
+            
+    # Create a new state dict removing '_orig_mod' prefix from checkpoint
+    # when using torch.compile with aot_eager backend, the keys have a prefix '_orig_mod.'
+    # we need, to remov this prefix to be able to load the model otherwise we have a mismatch
+    fixed_state_dict = {}
+    for key, value in checkpoint['model_state_dict'].items():
+        if key.startswith('_orig_mod.'):
+            new_key = key.replace('_orig_mod.', '')
+            fixed_state_dict[new_key] = value
+        else:
+            fixed_state_dict[key] = value
+
+    # Compare the shapes of tensors
+    for key in fixed_state_dict:
+        if key in model.state_dict():
+            ckpt_shape = fixed_state_dict[key].shape
+            model_shape = model.state_dict()[key].shape
+            if ckpt_shape != model_shape:
+                print(f"Shape mismatch for {key}: checkpoint {ckpt_shape} vs model {model_shape}")
+        else:
+            print(f"Key {key} not found in model state dict")
+
+    model.load_state_dict(fixed_state_dict)
 
     if optimizer is not None and 'optimizer_state_dict' in checkpoint:
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
@@ -108,42 +128,65 @@ def load_checkpoint(path, model, optimizer=None, scheduler=None, device="cpu"):
     else:
         raise ValueError("Scheduler state dict not found in checkpoint. Unable to load scheduler.")
     
-    
-    # Load statistics if they exist
-    stats = TrainingStats()
-    if 'stats_file' in checkpoint and os.path.exists(checkpoint['stats_file']):
-        stats.load_stats(checkpoint['stats_file'])
-    
-    return checkpoint['epoch'], checkpoint['step'], checkpoint['loss'], checkpoint['train_loader_state'], stats
+    return checkpoint['epoch'], checkpoint['step'], checkpoint['loss'], checkpoint['train_loader_state']
 
 def get_last_checkpoint(save_dir):
     """
     Get the last checkpoint file in the given directory.
     Returns the path to the last checkpoint file.
     """
-    checkpoints = sorted([f for f in os.listdir(save_dir) if f.startswith("checkpoint_")])
+    # check if directory exists
+    if not os.path.exists(save_dir):
+        return None
 
+    checkpoints = sorted([f for f in os.listdir(save_dir) if f.startswith("checkpoint_")])
+    if len(checkpoints) == 0:
+        return None
     return os.path.join(save_dir, checkpoints[-1])
 
 # -------------------------------------------------------------
 
+def log_print(msg):
+    print(msg)
+    log_file.write(msg + "\n")
+    log_file.flush()
 
 
 device = "cpu"
+backend = "inductor"
 if torch.cuda.is_available():
+    current_device = torch.cuda.current_device()
+    major, minor = torch.cuda.get_device_capability(current_device)
+    if major < 7:
+        backend = "aot_eager"
     device = "cuda"
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
-print(f"using device: {device}")
+
 device_type = "cuda" if device.startswith("cuda") else "cpu"
 
+# trying to not get out of memory error
+os.environ["PYTORCH_CUDA_ALLOC_CONF"]="expandable_segments:True"
+
+log_file = open("logs.txt", "a")
+stats_file = open("stats.txt", "a")
+
+# Write header for stats file (if it's empty)
+if stats_file.tell() == 0:
+    stats_file.write("epoch,step,loss,learning_rate\n")
+
+log_print("------------------------------------------------------------------------------------")
+# print current date YYYY-MM-DD-HH-MM-SS
+log_print(f"Date - time : {time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())}")
+log_print(f"using device: {device}")
+log_print(f"using backend: {backend}")
 B = 8
 T = GPT_CONFIG["context_length"]
 start_epoch = 0
 start_step = 0
-checkpoint = "checkpoints/checkpoint_0_3000_5.50.pt"
+
 useCheckpoint = True
-stats = TrainingStats()
+
 
 val_loader = DataLoaderLite(B, T, "valid")
 train_loader = DataLoaderLite(B, T, "train")
@@ -152,8 +195,8 @@ tokenizer = tiktoken.get_encoding("gpt2")
 # number of batches in one epoch
 nb_train_steps = int(HYPERS["tokens_per_shard"] / (B * T)) * len(train_loader.shards)
 nb_val_steps = int(len(val_loader.shards) * HYPERS["tokens_per_shard"] / (B * T))
-print(f"nb_train_steps: {nb_train_steps}")
-print(f"nb_val_steps: {nb_val_steps}")
+log_print(f"nb_train_steps: {nb_train_steps}")
+log_print(f"nb_val_steps: {nb_val_steps}")
 
 total_steps = nb_train_steps * HYPERS["epochs"]
 warmup_steps = total_steps // 10 # use 10% for warmup
@@ -166,19 +209,18 @@ scheduler = get_linear_schedule_with_warmup(optimizer, warmup_steps, total_steps
 
 if useCheckpoint:
     checkpoint = get_last_checkpoint("checkpoints")
-    if checkpoint is None:
-        raise ValueError("No checkpoint found")
-    start_epoch, start_step, loss, train_state, stats = load_checkpoint(checkpoint, model, optimizer, scheduler,device=device)
-    shard_index = train_state["shard_index"]
-    print(f"Loaded checkpoint: epoch {start_epoch}, step {start_step}, loss {loss} - shard: {shard_index}")
-    train_loader.set_state(train_state)
+    if checkpoint is not None:
+        start_epoch, start_step, loss, train_state = load_checkpoint(checkpoint, model, optimizer, scheduler,device=device)
+        shard_index = train_state["shard_index"]
+        log_print(f"Loaded checkpoint: epoch {start_epoch}, step {start_step}, loss {loss} - shard: {shard_index}")
+        train_loader.set_state(train_state)
+
+model = torch.compile(model, backend=backend)
 
 t0 = time.time()
 for epoch in range(start_epoch,HYPERS["epochs"]):
     for step in range(start_step, nb_train_steps):
-        if step > 6000:
-            break
-        if step > start_step + 1000:
+        if step > start_step + 2000:
             break
         model.train()
         x, y = train_loader.next_batch()
@@ -189,26 +231,50 @@ for epoch in range(start_epoch,HYPERS["epochs"]):
         optimizer.step() # update weights
         scheduler.step() # update learning rate
         optimizer.zero_grad() # reset gradients
-
         
         if step % 50 == 0:
-            print(f"step: {step}, loss: {loss.item()}")
-            stats.update(
-                step=step,
-                loss=loss.item(),
-                lr=scheduler.get_last_lr()[0],
-                shard_index=train_loader.current_shard_index
-            )
+            log_msg = f"step: {step}, loss: {loss.item()}"
+            log_print(log_msg)  
+            
+            # Write stats in CSV format for easy parsing
+            stats_msg = f"{epoch},{step},{loss.item()},{scheduler.get_last_lr()[0]}\n"
+            stats_file.write(stats_msg)
+            stats_file.flush()
+        
         if step % 500 == 0:
             text = generate_text_completion(model, "Je suis")
-            print(f"text: ///{text}///")
-            stats.update(step=step, generated_text=text)
-        if step % 500 == 0:
-            save_checkpoint(model, optimizer, scheduler, train_loader, GPT_CONFIG, epoch, step, loss.item(), stats,"checkpoints")
+            log_msg = f"{step}: Generated text: ///{text.strip()}///"
+            log_print(log_msg)
+        # evaluate validation loss
+        if step % 1000 == 0 and step > 0:
+            model.eval()
+            val_loader.reset()
+            with torch.no_grad():
+                val_loss_accum = 0.0
+                # average loss 
+                val_loss_steps = 2
+                for _ in range(val_loss_steps):
+                    x, y = val_loader.next_batch()
+                    x, y = x.to(device), y.to(device)
+                    with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                        logits = model(x)
+                    loss = torch.nn.functional.cross_entropy(logits.view(-1, GPT_CONFIG["vocab_size"]), y.view(-1))
+                    loss = loss / val_loss_steps
+                    val_loss_accum += loss.detach()
+                log_print(f"Validation loss: {val_loss_accum.item()}")
+        if step % 500 == 0 and step > 0:
+            save_checkpoint(model, optimizer, scheduler, train_loader, GPT_CONFIG, epoch, step, loss.item(), "checkpoints")
         
 t1 = time.time()
-print(f"Time: {t1-t0}")
-print(f"Tokens: {B}*{T}*{step} = {B*T*step}")
-print(f"Tokens/s: {(B*T*step)/(t1-t0)}")
-print(f"Loss: {loss.item()}")
-print(f"Shard index {train_loader.current_shard_index}")
+wrapup_message = f"""
+Time: {t1-t0}
+Processed Tokens: {B}*{T}*{step - start_step} = {B*T*(step - start_step)}
+Tokens/s: {(B*T*(step - start_step))/(t1-t0)}
+Loss: {loss.item()}
+Total Tokens: {B}*{T}*{step} = {B*T*step}
+Shard index: {train_loader.current_shard_index}
+"""
+log_print(wrapup_message)
+log_file.close()
+stats_file.close()
+
